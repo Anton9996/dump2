1547e5c0619ff58c2a61d456d89edfad5d9e87bf
Anton09 <korjant09996@gmail.com>
2022-12-02 17:57:51 +0400

    ia zdelal

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число N и выдает факториал этого числа
+Console.WriteLine("введите число и программа посчитает его факториал!");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int factorial(int num)
+{
+    int count = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        count = count * i;
+    }
+    return count;
+}
+int result = factorial(number);
+Console.WriteLine($"Факториал числа {number} = {result} ");
