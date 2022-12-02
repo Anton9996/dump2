a39b7a79920cef0d3a6a5579b4c2438476a6d404
Anton09 <korjant09996@gmail.com>
2022-12-02 17:27:58 +0400

    save32

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine($"!");
-int a = 231;
+﻿// Напишите программу, которая принимает на вход число и выдает кол-во цифр в числе
+// 456 -> 3
+Console.WriteLine("введите число и программа посчитает кол-во цифр в нем");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count(int num)
+{
+    int count = 0;
+    if (num == 0) return 1;
+    while (num > 0)
+    {
+        count++;
+        num = num / 10;
+    }
+    return count;
+}
+int result = count(number);
+Console.WriteLine($"в числе {number}, {result} ");
